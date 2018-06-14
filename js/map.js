4651bbf3ea4fe681ddda490b946c8b0823ff796e
Nikita Prokopenko <guardprokopenko@gmail.com>
2018-06-14 23:20:50 +0300

    Decomposition try

--- js/map.js
+++ js/map.js
@@ -24,6 +24,36 @@ var CARDS_QUANTITY = 8;
 var cycleCount;
 var cardData = [];
 
+var appartmentTypes = {
+  flat: 'Квартира',
+  bungalo: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+};
+
+// Removed fade class
+
+var map = document.querySelector('.map');
+var mapFilterContainer = map.querySelector('.map__filters-container');
+map.classList.remove('map--faded');
+
+// Map pin
+
+var mapPins = map.querySelector('.map__pins');
+var similarPinTemplate = document.querySelector('template').content.querySelector('.map__pin');
+
+var MAP_PIN_WIDTH_CENTER = 25;
+var MAP_PIN_HEIGHT = 70;
+
+// Map card
+
+var similarMapCardTemplate = document.querySelector('template').content.querySelector('.map__card.popup');
+var cardPhotoWidth = 45;
+var cardPhotoHeight = 40;
+var cardPhoto = document.createElement('img');
+
+// Test data generation
+
 function generateAvatar() {
   var count = cycleCount + 1;
   var avatarImage = AVATARS + count + '.png';
@@ -90,11 +120,9 @@ function generateFeatures() {
   var minFeatureCount = 0;
   var featureArrayLength = Math.floor(Math.random() * (maxFeatureCount - minFeatureCount)) + minFeatureCount;
   var offerFeatures = [];
-  
   for (var i = 0; i < featureArrayLength; i++) {
     offerFeatures.push(FEATURES[i]);
   }
-  
   return offerFeatures;
 }
 
@@ -116,129 +144,117 @@ function generateLocationY() {
 function createCard() {
   for (var i = 0; i < CARDS_QUANTITY; i++) {
     cycleCount = i;
-    
-    var obj = {
+    var cardDataItem = {
       'authors': {},
       'offer': {},
       'location': {},
     };
-    
-    obj.authors.avatar = generateAvatar();
-    obj.offer.title = generateTitle();
-    obj.offer.address = generateAddress();
-    obj.offer.price = generatePrice();
-    obj.offer.type = generateType();
-    obj.offer.rooms = generateRooms();
-    obj.offer.guests = generateGuests();
-    obj.offer.checkin = generateCheckin();
-    obj.offer.checkout = generateCheckout();
-    obj.offer.features = generateFeatures();
-    obj.offer.description = DESCRIPTION;
-    obj.offer.photos = generatePhotos();
-    obj.location.x = generateLocationX();
-    obj.location.y = generateLocationY();
-    
-    cardData.push(obj);
+    cardDataItem.authors.avatar = generateAvatar();
+    cardDataItem.offer.title = generateTitle();
+    cardDataItem.offer.address = generateAddress();
+    cardDataItem.offer.price = generatePrice();
+    cardDataItem.offer.type = generateType();
+    cardDataItem.offer.rooms = generateRooms();
+    cardDataItem.offer.guests = generateGuests();
+    cardDataItem.offer.checkin = generateCheckin();
+    cardDataItem.offer.checkout = generateCheckout();
+    cardDataItem.offer.features = generateFeatures();
+    cardDataItem.offer.description = DESCRIPTION;
+    cardDataItem.offer.photos = generatePhotos();
+    cardDataItem.location.x = generateLocationX();
+    cardDataItem.location.y = generateLocationY();
+    cardData.push(cardDataItem);
   }
   return cardData;
 }
 
+// Map pin creation
+
+function createPin() {
+  for (var i = 0; i < cardData.length; i++) {
+    var mapPin = similarPinTemplate.cloneNode(true);
+    var mapPinImage = mapPin.querySelector('img');
+    mapPin.style = 'left: ' + (cardData[i].location.x - MAP_PIN_WIDTH_CENTER) + 'px;' + ' top: ' + (cardData[i].location.y + MAP_PIN_HEIGHT) + 'px;';
+    mapPinImage.src = cardData[i].authors.avatar;
+    mapPinImage.alt = cardData[i].offer.title;
+    mapPins.appendChild(mapPin);
+  }
+  return mapPins;
+}
+
+// Map card template creation
+
+function createMapCardMainInfo() {
+  for (var i = 0; i < cardData.length; i++) {
+    var mapCard = similarMapCardTemplate.cloneNode(true);
+    var mapCardTitle = mapCard.querySelector('.popup__title');
+    var mapCardAddress = mapCard.querySelector('.popup__text--address');
+    var mapCardPrice = mapCard.querySelector('.popup__text--price');
+    var mapCardRoomsGuests = mapCard.querySelector('.popup__text--capacity');
+    var mapCardCheckinCheckout = mapCard.querySelector('.popup__text--time');
+    var mapCardDescription = mapCard.querySelector('.popup__description');
+    var mapCardAvatar = mapCard.querySelector('.popup__avatar');
+    mapCardTitle.innerHTML = cardData[i].offer.title;
+    mapCardAddress.innerHTML = cardData[i].offer.address;
+    mapCardPrice.innerHTML = cardData[i].offer.price + '₽/ночь';
+    mapCardRoomsGuests.innerHTML = cardData[i].offer.rooms + ' комнаты для ' + cardData[i].offer.guests + ' гостей';
+    mapCardCheckinCheckout.innerHTML = 'Заезд после ' + cardData[i].offer.checkin + ', выезд до ' + cardData[i].offer.checkout;
+    mapCardDescription.innerHTML = cardData[i].offer.description;
+    mapCardAvatar.src = cardData[i].authors.avatar;
+  }
+  return mapCard;
+}
+
+function createMapCardType() {
+  for (var i = 0; i < cardData.length; i++) {
+    var mapCard = similarMapCardTemplate.cloneNode(true);
+    var mapCardType = mapCard.querySelector('.popup__type');
+    mapCardType.innerHTML = appartmentTypes[cardData[i].offer.type];
+  }
+  return mapCard;
+}
+
+function createMapCardFeature() {
+  for (var i = 0; i < cardData.length; i++) {
+    var mapCard = similarMapCardTemplate.cloneNode(true);
+    var mapCardFeatures = mapCard.querySelector('.popup__features');
+    mapCardFeatures.innerHTML = '';
+    var feature = document.createElement('li');
+    feature.className = 'popup__feature popup__feature--' + cardData[i].offer.features[i];
+    mapCardFeatures.appendChild(feature);
+  }
+  return mapCard;
+}
+
+function createMapCardPhotos() {
+  for (var i = 0; i < cardData.length; i++) {
+    var mapCard = similarMapCardTemplate.cloneNode(true);
+    var mapCardPhotos = mapCard.querySelector('.popup__photos');
+    for (var y = 0; y < cardData[i].offer.photos.length; y++) {
+      mapCardPhotos.innerHTML = '';
+      cardPhoto.alt = 'Фотография жилья';
+      cardPhoto.width = cardPhotoWidth;
+      cardPhoto.height = cardPhotoHeight;
+      cardPhoto.className = 'popup__photo';
+      cardPhoto.src = cardData[i].offer.photos[y];
+      mapCardPhotos.appendChild(cardPhoto);
+    }
+  }
+  return mapCard;
+}
+
+function createMapCard() {
+  for (var i = 0; i < cardData.length; i++) {
+    var mapCard = similarMapCardTemplate.cloneNode(true);
+    createMapCardMainInfo();
+    createMapCardType();
+    createMapCardFeature();
+    createMapCardPhotos();
+    map.insertBefore(mapCard, mapFilterContainer);
+  }
+  return mapCard;
+}
+
 createCard();
-
-// Removed fade class
-
-var map = document.querySelector('.map');
-var mapFilterContainer = document.querySelector('.map__filters-container');
-map.classList.remove('map--faded');
-//
-// // Map pin creation
-//
-// var mapPins = document.querySelector('.map__pins');
-// var similarPinTemplate = document.querySelector('template').content.querySelector('.map__pin');
-//
-// function mapPinCreate() {
-//   mapPin.style = 'left: ' + (ad.location.x - 25) + 'px;' + ' top: ' + (ad.location.y + 70) + 'px;';
-//   mapPinImage.src = ad.authors.avatar;
-//   mapPinImage.alt = ad.offer.title;
-//   return mapPins.appendChild(mapPin);
-// }
-//
-// // Map card template & constructor
-//
-// var similarMapCardTemplate = document.querySelector('template').content.querySelector('.map__card.popup');
-//
-// function mapCardCreate() {
-//   mapCardTitle.innerHTML = ad.offer.title;
-//   mapCardAddress.innerHTML = ad.offer.address;
-//   mapCardPrice.innerHTML = ad.offer.price + '₽/ночь';
-//   mapCardRoomsGuests.innerHTML = ad.offer.rooms + ' комнаты для ' + ad.offer.guests + ' гостей';
-//   mapCardCheckinCheckout.innerHTML = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
-//   mapCardDescription.innerHTML = ad.offer.description;
-//   mapCardAvatar.src = ad.authors.avatar;
-//
-//   switch (ad.offer.type) {
-//     case 'flat':
-//       mapCardType.innerHTML = 'Квартира';
-//       break;
-//     case 'bungalo':
-//       mapCardType.innerHTML = 'Бунгало';
-//       break;
-//     case 'house':
-//       mapCardType.innerHTML = 'Дом';
-//       break;
-//     case 'palace':
-//       mapCardType.innerHTML = 'Дворец';
-//       break;
-//     default:
-//       mapCardType.innerHTML = 'Жилье';
-//   }
-//
-//   for (var x = 0; x < ad.offer.features.length; x++) {
-//     var feature = document.createElement('li');
-//     feature.className = 'popup__feature popup__feature--' + ad.offer.features[x];
-//     mapCardFeatures.appendChild(feature);
-//   }
-//
-//   for (var y = 0; y < ad.offer.photos.length; y++) {
-//     var photo = document.createElement('img');
-//     photo.alt = 'Фотография жилья';
-//     photo.width = '45';
-//     photo.height = '40';
-//     photo.className = 'popup__photo';
-//     photo.src = ad.offer.photos[y];
-//     mapCardPhotos.appendChild(photo);
-//   }
-//
-//   return mapCard;
-// }
-//
-// // Map card creation cycle
-//
-// for (var i = 0; i < ads.length; i++) {
-//
-//   var ad = ads[i];
-//
-//   var mapPin = similarPinTemplate.cloneNode(true);
-//   var mapPinImage = mapPin.querySelector('img');
-//   mapPinCreate();
-//
-//   // Define map card parts
-//
-//   var mapCard = similarMapCardTemplate.cloneNode(true);
-//   var mapCardTitle = mapCard.querySelector('.popup__title');
-//   var mapCardAddress = mapCard.querySelector('.popup__text--address');
-//   var mapCardPrice = mapCard.querySelector('.popup__text--price');
-//   var mapCardType = mapCard.querySelector('.popup__type');
-//   var mapCardRoomsGuests = mapCard.querySelector('.popup__text--capacity');
-//   var mapCardCheckinCheckout = mapCard.querySelector('.popup__text--time');
-//   var mapCardFeatures = mapCard.querySelector('.popup__features');
-//   var mapCardDescription = mapCard.querySelector('.popup__description');
-//   var mapCardPhotos = mapCard.querySelector('.popup__photos');
-//   var mapCardAvatar = mapCard.querySelector('.popup__avatar');
-//   mapCardFeatures.innerHTML = '';
-//   mapCardPhotos.innerHTML = '';
-//
-//   mapCardCreate();
-//
-//   map.insertBefore(mapCard, mapFilterContainer);
-// }
+createPin();
+createMapCard();
