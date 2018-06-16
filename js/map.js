e002dd32e215801858ccb824b4449a2fd7feb7e8
Nikita Prokopenko <guardprokopenko@gmail.com>
2018-06-16 11:59:06 +0300

    Review comments

--- js/map.js
+++ js/map.js
@@ -2,8 +2,13 @@
 
 var AVATARS = 'img/avatars/user0';
 var TITLES = [
-  'Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец',
-  'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря',
+  'Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря',
   'Неуютное бунгало по колено в воде'
 ];
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
@@ -12,7 +17,8 @@ var CHECKOUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var DESCRIPTION = '';
 var PHOTOS = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 var MIN_LOCATION_X = 300;
@@ -21,11 +27,14 @@ var MIN_LOCATION_Y = 130;
 var MAX_LOCATION_Y = 630;
 var CARDS_QUANTITY = 8;
 
+// TODO Create object with const
+var MAP_PIN_WIDTH_CENTER = 25;
+var MAP_PIN_HEIGHT = 70;
 
 var cycleCount;
 var cardData = [];
 
-var appartmentTypes = {
+var AppartmentTypes = {
   flat: 'Квартира',
   bungalo: 'Бунгало',
   house: 'Дом',
@@ -43,28 +52,25 @@ map.classList.remove('map--faded');
 var mapPins = map.querySelector('.map__pins');
 var similarPinTemplate = document.querySelector('template').content.querySelector('.map__pin');
 
-var MAP_PIN_WIDTH_CENTER = 25;
-var MAP_PIN_HEIGHT = 70;
-
 // Map card
 
 var similarMapCardTemplate = document.querySelector('template').content.querySelector('.map__card.popup');
+// TODO Create object with const
 var cardPhotoWidth = 45;
 var cardPhotoHeight = 40;
 var cardPhoto = document.createElement('img');
 
 // Test data generation
 
-function generateAvatar() {
-  var count = cycleCount + 1;
-  var avatarImage = AVATARS + count + '.png';
-  return avatarImage;
+function generateAvatar(avatarNumber) {
+  return AVATARS + avatarNumber + '.png';
 }
 
-function generateTitle() {
-  var cardTitle = TITLES[cycleCount];
-  return cardTitle;
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
+
+
 
 function generateAddress() {
   var minCoordinate = 1;
@@ -150,32 +156,49 @@ function createCard() {
       'offer': {},
       'location': {},
     };
-    cardDataItem.authors.avatar = generateAvatar();
-    cardDataItem.offer.title = generateTitle();
+    cardDataItem.authors.avatar = generateAvatar(i);
+    cardDataItem.offer.title = TITLES[getRandomInt(0, TITLES.length - 1)];
     cardDataItem.offer.address = generateAddress();
-    cardDataItem.offer.price = generatePrice();
-    cardDataItem.offer.type = generateType();
-    cardDataItem.offer.rooms = generateRooms();
-    cardDataItem.offer.guests = generateGuests();
-    cardDataItem.offer.checkin = generateCheckin();
+    cardDataItem.offer.price = getRandomInt(1000, 1000000);
+    cardDataItem.offer.type = generateType(); // См.стр. 160
+    cardDataItem.offer.rooms = generateRooms(); // аналогично
+    cardDataItem.offer.guests = generateGuests(); // аналогично
+    cardDataItem.offer.checkin = generateCheckin(); // аналогично
     cardDataItem.offer.checkout = generateCheckout();
-    cardDataItem.offer.features = generateFeatures();
+    cardDataItem.offer.features = generateFeatures(); // getRandomElementsFromCollection
     cardDataItem.offer.description = DESCRIPTION;
-    cardDataItem.offer.photos = generatePhotos();
-    cardDataItem.location.x = generateLocationX();
-    cardDataItem.location.y = generateLocationY();
+    cardDataItem.offer.photos = generatePhotos();  // Посмотри алгоритм Фишера-Йетса (https://webformyself.com/sortirovka-massivov-v-javascript/)
+    cardDataItem.location.x = generateLocationX(); // getRandomInt
+    cardDataItem.location.y = generateLocationY(); // getRandomInt
     cardData.push(cardDataItem);
   }
   return cardData;
 }
 
+function getRandomElementsFromCollection (count, sourceCollection) {
+ // Цикл
+  // Получение элементов
+  // Возвращать массив
+}
+
+// getRandomElementsFromCollection(2, PHOTOS)
+
 // Map pin creation
 
 function createPin() {
-  for (var i = 0; i < cardData.length; i++) {
+  // TODO Посмотреть document.createDocumentFragment();
+  // TODO Список пинов для рендеринга передавать через параметр
+   for (var i = 0; i < cardData.length; i++) {
     var mapPin = similarPinTemplate.cloneNode(true);
     var mapPinImage = mapPin.querySelector('img');
+
+    // TODO. Расчет координат в создание объекта
     mapPin.style = 'left: ' + (cardData[i].location.x - MAP_PIN_WIDTH_CENTER) + 'px;' + ' top: ' + (cardData[i].location.y + MAP_PIN_HEIGHT) + 'px;';
+
+    // TODO. Используй style в объектном стиле
+    // mapPin.style.top = location.top + 'px';
+    // mapPin.style.left = location.x + 'px';
+
     mapPinImage.src = cardData[i].authors.avatar;
     mapPinImage.alt = cardData[i].offer.title;
     mapPins.appendChild(mapPin);
@@ -186,6 +209,13 @@ function createPin() {
 // Map card template creation
 
 function createMapCardMainInfo() {
+  // TODO. Избавиться от цикла. Формируем только одну
+  // TODO. В параметр передаем один offerObject.
+
+  // var newOfferCard = template.querySelector('.map__card');
+  // newOfferCard.querySelector('.popup__avatar').src =offerObject.author.avatar;
+  // newOfferCard.querySelector('.popup__text--address').textContent = offerObject.offer.title;
+
   for (var i = 0; i < cardData.length; i++) {
     var mapCard = similarMapCardTemplate.cloneNode(true);
     var mapCardTitle = mapCard.querySelector('.popup__title');
@@ -210,7 +240,7 @@ function createMapCardType() {
   for (var i = 0; i < cardData.length; i++) {
     var mapCard = similarMapCardTemplate.cloneNode(true);
     var mapCardType = mapCard.querySelector('.popup__type');
-    mapCardType.innerHTML = appartmentTypes[cardData[i].offer.type];
+    mapCardType.innerHTML = AppartmentTypes[cardData[i].offer.type];
   }
   return mapCard;
 }
@@ -256,6 +286,12 @@ function createMapCard() {
   return mapCard;
 }
 
+
 createCard();
 createPin();
 createMapCard();
+
+// TODO. Проверить вызов
+
+var allPins = generateObjects(); // Массив из 8 пинов
+createCard(allPins[0]);
