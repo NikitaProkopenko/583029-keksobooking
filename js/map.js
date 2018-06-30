19e96ef1138d634ece5354be0f7fa58dc10f686c
Nikita Prokopenko <guardprokopenko@gmail.com>
2018-06-30 20:06:28 +0300

    Move functionality for the main pin

--- js/map.js
+++ js/map.js
@@ -423,3 +423,51 @@ adFormReset.addEventListener('click', function () {
   disableFormsArray(pageFieldsetArray);
   removePin();
 });
+
+// module5-task1
+
+mainMapPin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var dragged = false;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    mainMapPin.style.top = (mainMapPin.offsetTop - shift.y) + 'px';
+    mainMapPin.style.left = (mainMapPin.offsetLeft - shift.x) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (dragged) {
+      var onClickPreventDefault = function (evt) {
+        evt.preventDefault();
+        mainMapPin.removeEventListener('click', onClickPreventDefault);
+      };
+      mainMapPin.addEventListener('click', onClickPreventDefault);
+    }
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
